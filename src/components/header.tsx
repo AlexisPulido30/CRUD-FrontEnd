1271853221e61639dfe6db8cca90bcb886b6c92e
AlexisPulido30 <alexispulido.agn@outlook.com>
2025-12-01 14:43:36 -0600

    Se agrego funcionalidad a el boton de cerrar sesion

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,14 @@
+import { useQueryClient } from '@tanstack/react-query'
+
 export default function Header() {
+     const queryClient = useQueryClient()
+
+     const logout = () =>{
+      localStorage.removeItem('AUTH_TOKEN')
+      queryClient.invalidateQueries({queryKey: ['user']})
+     }
   return (
+
     <header className="flex items-center justify-between px-6 py-3 text-white bg-gray-900 shadow-md">
       {/* Icono de programador */}
       <div className="flex items-center space-x-2">
@@ -8,7 +17,8 @@ export default function Header() {
       </div>
 
       {/* Botón cerrar sesión */}
-      <button className="px-4 py-2 text-sm font-medium transition-all bg-red-600 rounded-lg hover:bg-red-700">
+      <button className="px-4 py-2 text-sm font-medium transition-all bg-red-600 rounded-lg hover:bg-red-700"
+      onClick={logout}>
         Cerrar sesión
       </button>
     </header>
